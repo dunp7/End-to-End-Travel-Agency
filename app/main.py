655f89e4c8a7b0bfa34b10cc56e8401e855d24ad
Phạm Tiến Dũng  (HN-IID-Sales Executive) <dungpt6@runsystem.local>
2025-06-11 15:36:52 +0700

    project structure

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from app.routers import destinations, itineraries, transport, accommodations
+from app.routers import destinations, itineraries, transports, accomodations
 
 
 app = FastAPI(title= "Travel Bot API", version="1.0.0")
@@ -8,12 +8,12 @@ app = FastAPI(title= "Travel Bot API", version="1.0.0")
 # Routers
 app.include_router(destinations.router, prefix="/destinations", tags=["Destinations"])
 app.include_router(itineraries.router, prefix="/itineraries", tags=["Itineraries"])
-app.include_router(transport.router, prefix="/transport", tags=["Transport"])
-app.include_router(accommodations.router, prefix="/accommodations", tags=["Accommodations"])
+app.include_router(transports.router, prefix="/transports", tags=["Transport"])
+app.include_router(accomodations.router, prefix="/accommodations", tags=["Accommodations"])
 
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=5000, reload=True) 
+    uvicorn.run("main:app", host="0.0.0.0", port=9999, reload=True) 
 
-# http://127.0.0.1:5000 to access API
+# http://127.0.0.1:9999 to access API
